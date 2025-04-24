9e81d4d5fd68192beb2bc053ded7333e4cf494d7
Elizaveta <elizabess27@mail.ru>
2025-04-24 20:23:47 +0300

    1

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import Board from '../components/board/Board';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const content = document.querySelector('.content');
-  const board = new Board(content);
-  board.bindToDOM();
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,28 @@
-const path = require('node:path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: { loader: 'babel-loader' },
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
+    module: {
+        rules: [
+            {
+                test: /.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
